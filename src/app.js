55858e2e0938e317e21d2c98e12465815ac0caf5
YukiZiel <97055206+YukiZiel@users.noreply.github.com>
2023-04-20 14:36:43 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
+const commentForm = document.getElementById("comment-form");
+const commentsContainer = document.getElementById("comments-container");
 
+commentForm.addEventListener("submit", (event) => {
+  event.preventDefault(); // Prevenir que se envíe el formulario
+  
+  // Obtener los valores del formulario
+  const name = commentForm.elements["name"].value;
+  const comment = commentForm.elements["comment"].value;
+
+  // Crear un elemento para mostrar el comentario
+  const commentElement = document.createElement("div");
+  commentElement.classList.add("comment");
+  commentElement.innerHTML = `
+    <strong>${name}:</strong>
+    <div class="comment-text">${comment}</div>
+  `;
+
+  // Añadir el comentario al contenedor
+  commentsContainer.appendChild(commentElement);
+
+  // Limpiar el formulario
+  commentForm.reset();
+});

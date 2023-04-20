3b8f5f3163ad85cebd8654611705f3d7a83e896a
YukiZiel <97055206+YukiZiel@users.noreply.github.com>
2023-04-20 17:16:32 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,24 +1,24 @@
-const commentForm = document.getElementById("comment-form");
-const commentsContainer = document.getElementById("comments-container");
 
-commentForm.addEventListener("submit", (event) => {
-  event.preventDefault(); // Prevenir que se envíe el formulario
-  
-  // Obtener los valores del formulario
-  const name = commentForm.elements["name"].value;
-  const comment = commentForm.elements["comment"].value;
 
-  // Crear un elemento para mostrar el comentario
-  const commentElement = document.createElement("div");
-  commentElement.classList.add("comment");
-  commentElement.innerHTML = `
-    <strong>${name}:</strong>
-    <p class="comment-text">${comment}</p>
-  `;
+const commentForms = document.getElementsByClassName("comment-form");
 
-  // Añadir el comentario al contenedor
-  commentsContainer.appendChild(commentElement);
+for (let i = 0; i < commentForms.length; i++) {
+  commentForms[i].addEventListener("submit", (event) => {
+    event.preventDefault();
+    const name = commentForms[i].elements["name"].value;
+    const comment = commentForms[i].elements["comment"].value;
 
-  // Limpiar el formulario
-  commentForm.reset();
-});
+    const commentsContainer = commentForms[i].nextElementSibling;
+
+    const commentElement = document.createElement("div");
+    commentElement.classList.add("comment");
+    commentElement.innerHTML = `
+      <strong>${name}:</strong>
+      <p class="comment-text">${comment}</p>
+    `;
+
+    commentsContainer.appendChild(commentElement);
+
+    commentForms[i].reset();
+  });
+}
